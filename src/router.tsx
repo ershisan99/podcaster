6800f42b3f8c020b2f84bfe03de0b756fb928f3b
andres <andres99.dev@gmail.com>
2024-04-20 23:03:02 +0200

    wip: add layout component and wrap the app

--- src/router.tsx
+++ src/router.tsx
@@ -3,23 +3,29 @@ import { Home } from "./pages/home";
 import { Podcast } from "./pages/podcast";
 import { Episode } from "./pages/episode";
 import { PodcastEpisodesList } from "./components/podcast-episodes-list";
+import { Layout } from "./components/layout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/podcast",
-    element: <Podcast />,
+    element: <Layout />,
     children: [
       {
-        path: ":podcastId",
-        element: <PodcastEpisodesList />,
+        path: "/",
+        element: <Home />,
       },
       {
-        path: ":podcastId/episode/:episodeId",
-        element: <Episode />,
+        path: "/podcast",
+        element: <Podcast />,
+        children: [
+          {
+            path: ":podcastId",
+            element: <PodcastEpisodesList />,
+          },
+          {
+            path: ":podcastId/episode/:episodeId",
+            element: <Episode />,
+          },
+        ],
       },
     ],
   },
